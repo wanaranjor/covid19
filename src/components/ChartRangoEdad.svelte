0516f16899b8151c8f39ac976c8ef46e86aca659
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2020-04-29 20:58:50 -0500

    update charts and services

---
+++ src/components/ChartRangoEdad.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { createChartRangoEdad } from "../services/charts.js";
+	import { onMount } from "svelte";
+
+	export let totalRangoCasa;
+	export let totalRangoRecuperado;
+	export let totalRangoFallecido;
+	export let totalRangoHospital;
+	export let totalRangoUci;
+
+	onMount(async () => {
+		createChartRangoEdad(
+			totalRangoCasa,
+			totalRangoRecuperado,
+			totalRangoFallecido,
+			totalRangoHospital,
+			totalRangoUci
+		);
+	});
+</script>
+
+<div class="bg-gray-900 border border-gray-700 rounded">
+	<canvas id="chartRangoEdad" width="800" height="400"></canvas>
+</div>
